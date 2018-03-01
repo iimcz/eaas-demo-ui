9c402ea03cbddc2fc7e35fbc94c4cc3704f128b7
Oleg Zharkov <jos.to.new@gmail.com>
2018-03-01 18:45:32 +0100

    eaas-client update

--- admin/app.js
+++ admin/app.js
@@ -1537,7 +1537,7 @@
                                         this.confirmed = function()
                                         {
                                             window.onbeforeunload = null;
-                                            window.eaasClient.release();
+                                            // window.eaasClient.release();
                                             $('#emulator-stopped-container').show();
 
                                             if($stateParams.isTestEnv)
