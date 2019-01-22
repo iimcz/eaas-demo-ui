bce76340138d314fdfd4fc2aa5c61a0f49a19e15
Oleg Zharkov <jos.to.new@gmail.com>
2019-01-22 19:46:24 +0100

    Polishing of Software grid

--- eaas-frontend-admin/src/app/modules/software/overview.ctrl.js
+++ eaas-frontend-admin/src/app/modules/software/overview.ctrl.js
@@ -4,34 +4,25 @@ module.exports = ['softwareList', '$scope', '$stateParams', function (softwareLi
 
    console.log(vm.swList);
 
+    $scope.onPageSizeChanged = function() {
+        $scope.gridOptions.api.paginationSetPageSize(Number(vm.pageSize));
+    };
+
     vm.initColumnDefs = function () {
         return [
-            {headerName: '', width: 41, checkboxSelection: true, suppressSorting: true,
-                suppressMenu: true},
             {headerName: "ID", field: "id"},
             {headerName: "Label", field: "label"},
             {headerName: "Operating System", field: "isOperatingSystem"},
-            {headerName: "Edit", field: "edit", cellRenderer: editBtnRenderer, suppressSorting: true,
+            {headerName: "", field: "edit", cellRenderer: editBtnRenderer, suppressSorting: true,
                 suppressMenu: true}
         ];
     };
 
     function editBtnRenderer(params) {
-        params.$scope.switchAction = switchAction;
         params.$scope.selected = $scope.selected;
-        params.$scope.landingPage = vm.landingPage;
-
-        return  `<button id="single-button" type="button" class="dropbtn">
-                  {{'CHOOSE_ACTION'| translate}} <span class="caret"></span>
+        return  `<button id="single-button" ui-sref="admin.sw-ingest({swId: data.id})" type="button" class="dropbtn">
+                  {{'SW_OVERVIEW_SW_EDIT'| translate}}
                 </button>`;
-
-    }
-
-    function onRowSelected(event) {
-        if ($scope.gridOptions.api.getSelectedRows().length > 0)
-            $('#overviewDeleteButton').show();
-        else
-            $('#overviewDeleteButton').hide();
     }
 
     $scope.gridOptions = {
@@ -46,7 +37,6 @@ module.exports = ['softwareList', '$scope', '$stateParams', function (softwareLi
         enableSorting: true,
         enableFilter: true,
         enableCellChangeFlash: true,
-        onRowSelected: onRowSelected,
         suppressRowClickSelection: true,
         domLayout: 'autoHeight',
         suppressHorizontalScroll: true,
