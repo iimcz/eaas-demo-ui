df7935ef561ecc2114b35b810d91a5866896008f
Klaus Rechert <klaus@openslx.com>
2019-02-01 12:23:29 +0100

    add column date (for hidden archive) to make select button work

--- eaas-frontend-admin/src/app/modules/environments/overview.ctrl.js
+++ eaas-frontend-admin/src/app/modules/environments/overview.ctrl.js
@@ -288,7 +288,7 @@ module.exports = ['$rootScope', '$http', '$state', '$scope', '$stateParams', 'en
                     if((element.archive == 'default' && vm.viewArchive === 0) ||
                         (element.archive == "public" && vm.viewArchive === 1) ||
                         (element.archive == "remote" && vm.viewArchive === 2))
-                            rowData.push({name: element.title, id: element.envId, owner: (element.owner) ? element.owner : "shared"});
+                            rowData.push({name: element.title, id: element.envId, archive: element.archive, owner: (element.owner) ? element.owner : "shared"});
                 })
             else if (vm.view == 1) {
                 vm.envs.forEach(function (element) {
