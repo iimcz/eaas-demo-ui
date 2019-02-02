181520e8062f77a17f7933cac6d92c2696b1487f
Klaus Rechert <klaus@openslx.com>
2019-02-02 19:50:13 +0100

    multiple metadata sources hack

--- eaas-frontend-admin/src/app/modules/metadata/metadata.ctrl.js
+++ eaas-frontend-admin/src/app/modules/metadata/metadata.ctrl.js
@@ -2,7 +2,18 @@ module.exports = ['$scope' , '$state', '$stateParams', 'oaiHarvesterList', '$tra
     function($scope, $state, $stateParams, oaiHarvesterList, $translate, $http, localConfig, growl, $interval, helperFunctions, $uibModal, REST_URLS) {
 
     var vm = this;
-    vm.oaiHarvesterList = oaiHarvesterList.data;
+
+    vm.oaiHarvesterList = [];
+    if(oaiHarvesterList.data)
+    {
+        oaiHarvesterList.data.forEach(function(element){
+            console.log(element);
+            var names = element.split('-');
+            console.log(names);
+            if(vm.oaiHarvesterList.indexOf(names[0]) < 0)
+                vm.oaiHarvesterList.push(names[0]);
+        });
+    }
 
     vm.sync = function(harvester)
     {
@@ -12,8 +23,10 @@ module.exports = ['$scope' , '$state', '$stateParams', 'oaiHarvesterList', '$tra
              backdrop: 'static',
              template: require('./modals/wait.html')
         });
-        $http.post(localConfig.data.oaipmhServiceBaseUrl + "harvesters/" +  harvester).then(function(response) {
-            modal.close();
+        $http.post(localConfig.data.oaipmhServiceBaseUrl + "harvesters/" +  harvester + "-images").then(function(response) {
+            $http.post(localConfig.data.oaipmhServiceBaseUrl + "harvesters/" +  harvester + "-environments").then(function(response) {
+                modal.close();
+            });
         });
     }
 
@@ -23,28 +36,37 @@ module.exports = ['$scope' , '$state', '$stateParams', 'oaiHarvesterList', '$tra
             animation: true,
             template: require('./modals/addEndpoint.html'),
             controller: ["$scope", function($scope) {
-                this.confirmed = function(p)
+                var _this = this;
+                _this.success = false;
+                this.confirmed = function()
                 {
-                    var data = {};
-                    data.name = this.name;
-                    data.source = {};
-                    data.source.url = this.host + "/" + p;
-                    data.sink = {}
-                    data.sink.base_url = localConfig.data.eaasBackendURL + "metadata-repositories/remote";
-                    console.log(data);
+                    console.log(_this.providers);
+                    _this.name = _this.name.replace("-", "_");
+                    _this.providers.forEach(function(p) {
+                        // we only support images and environments
+                        if(p === 'images' || p === 'environments' ) {
+                            var data = {};
+                            data.name = _this.name + "-" + p;
+                            data.source = {};
+                            data.source.url = _this.host + "/" + p;
+                            data.sink = {}
+                            data.sink.base_url = localConfig.data.eaasBackendURL + "metadata-repositories/remote-" + p;
+                            console.log(data);
 
-                    $http.post(localConfig.data.oaipmhServiceBaseUrl +  "harvesters/", data).then(function() {
-                        $state.go('admin.metadata', {}, {reload: true});
+                            $http.post(localConfig.data.oaipmhServiceBaseUrl +  "harvesters/", data).then(function() {
+                                $state.go('admin.metadata', {}, {reload: true});
+                            });
+                       }
                     });
                 }
 
-                var _this = this;
                 this.resolve = function()
                 {
                     $http.get(_this.host)
                       .then(function(response) {
                         _this.providers = response.data;
-                        console.log(_this.providers);
+                        if(_this.providers.length >= 2)
+                            _this.success = true;
                       });
                 }
             }],
