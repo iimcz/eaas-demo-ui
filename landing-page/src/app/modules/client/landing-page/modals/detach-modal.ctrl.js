ae6921018778f40cd83fbef50939ab071f28ec96
Klaus Rechert <klaus@openslx.com>
2019-04-14 20:57:43 +0200

    typos

--- landing-page/src/app/modules/client/landing-page/modals/detach-modal.ctrl.js
+++ landing-page/src/app/modules/client/landing-page/modals/detach-modal.ctrl.js
@@ -14,11 +14,13 @@ module.exports = ['$state', '$http', '$scope', '$uibModal', 'currentEnv' , 'loca
         }
 
         this.detachTime;
+        this.sessionName;
         this.detach = function () {
             let url = localConfig.data.eaasBackendURL + formatStr("/sessions/{0}/detach", eaasClient.networkId);
             $http.post(url, {
                 lifetime: this.detachTime,
                 lifetime_unit: "minutes",
+                sessionName: this.sessionName
             }).then(function (response) {
                 modalCtrl.detachedComponentId = eaasClient.networkId;
                 if (response.status === 204) {
@@ -26,7 +28,7 @@ module.exports = ['$state', '$http', '$scope', '$uibModal', 'currentEnv' , 'loca
                     window.onbeforeunload = function () {
                         eaasClient.disconnect();
                     }.bind(eaasClient);
-                    growl.success('Session detached! );
+                    growl.success('Session detached!');
                     $state.go('admin.standard-envs-overview', {showObjects: false}, {reload: false});
                 } else {
                     growl.error('Unfortunately, this session can not be detached!');
