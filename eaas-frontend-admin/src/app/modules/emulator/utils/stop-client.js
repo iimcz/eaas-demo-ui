eac8c0a5b2ff0e89a7d54bf46982c78b42eeab29
Klaus Rechert <klaus@openslx.com>
2019-09-24 16:51:04 +0200

    do not release session

--- eaas-frontend-admin/src/app/modules/emulator/utils/stop-client.js
+++ eaas-frontend-admin/src/app/modules/emulator/utils/stop-client.js
@@ -55,9 +55,9 @@ export async function stopClient($uibModal, onlyDisconnect, download, eaasClient
 
             if (doDownload)
                 await f();
-            eaasClient.deleteOnUnload = true;
-            window.onbeforeunload = null;
-            await _stop(true);
+            // eaasClient.deleteOnUnload = true;
+            // window.onbeforeunload = null;
+            // await _stop(true);
             
         });
     } else {
