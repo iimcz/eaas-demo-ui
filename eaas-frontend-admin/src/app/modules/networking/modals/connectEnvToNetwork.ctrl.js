747b8df79aeddcb502684dfc356515102aad5c34
Klaus Rechert <klaus@openslx.com>
2020-06-26 11:19:56 +0200

    show only public envs for network connect

--- eaas-frontend-admin/src/app/modules/networking/modals/connectEnvToNetwork.ctrl.js
+++ eaas-frontend-admin/src/app/modules/networking/modals/connectEnvToNetwork.ctrl.js
@@ -1,6 +1,6 @@
 module.exports = ['$state', '$rootScope', '$http', '$scope', '$translate', 'growl', 'environments', 'sessionId', 'localConfig', 'REST_URLS', 'helperFunctions',
     function ($state, $rootScope, $http, $scope, $translate, growl, environments, sessionId, localConfig, REST_URLS, helperFunctions) {
-        this.environments = environments;
+        this.environments = environments.filter( (env) => env.archive === 'public');
 
         this.connectEnvironmentToNetwork = (env) => {
             window.open(localConfig.data.landingPage + "?sessionId=" + sessionId + "&connectEnvId=" + env.envId + "#!/attach-landing-page")
