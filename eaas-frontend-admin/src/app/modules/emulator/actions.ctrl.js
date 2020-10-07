61806742b438de75bbb7014c7f8f7659098d886a
Klaus Rechert <klaus@openslx.com>
2020-10-07 20:24:10 +0200

    fix typo

--- eaas-frontend-admin/src/app/modules/emulator/actions.ctrl.js
+++ eaas-frontend-admin/src/app/modules/emulator/actions.ctrl.js
@@ -258,7 +258,7 @@ module.exports = ['$rootScope', '$scope', '$state', '$uibModal', '$stateParams',
                     $("html, body").addClass("wait");
                     try {
                         let result = await eaasClient.getActiveSession().changeMedia(postObj);
-                        console.loh("change media result");
+                        console.log("change media result");
                         console.log(result);
                         growl.success($translate.instant('JS_MEDIA_CHANGETO') + mediaItem.chosen_medium_label);
                         mediaItem.currentMediumLabel = mediaItem.chosen_medium_label;
