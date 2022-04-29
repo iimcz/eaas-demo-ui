1d85f8a9d3a22d70826354297fa9772dbabcd641
Rafael Gieschke <rafael@gieschke.de>
2022-04-29 15:57:02 +0200

    build: add script to get config.json for debugging

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+selfdir="$(dirname -- "$(realpath -- "$0")")"
+cd -- "$selfdir"
+
+CONFIG_JSON_URL="${1-}"
+
+echoexec() {
+  printf '%s\n' "$*"
+  "$@"
+}
+
+if ! test "$CONFIG_JSON_URL"; then
+  printf "URL of instance (will be used to get config.json): "
+  read -r CONFIG_JSON_URL
+fi
+
+if test "$CONFIG_JSON_URL"; then
+  CONFIG_JSON_URL="${CONFIG_JSON_URL#http*://}"
+  CONFIG_JSON_URL="${CONFIG_JSON_URL%%/*}"
+  CONFIG_JSON_URL="https://$CONFIG_JSON_URL/admin/config.json"
+  echoexec curl -Lo eaas-frontend-admin/src/public/config.json  -- "$CONFIG_JSON_URL"
+fi
+
+npm ci
+cd eaas-frontend-admin
+npm ci
+npm start
